b513188d45e5354e5e8cc9ab62e5880904131b29
Wasil <WasiliyLolita6@gmail.com>
2023-06-06 01:25:04 +0300

    MainBuild 1.0

--- PrgAndroidApps/LimeApp/app/src/main/java/com/example/limeapp/PriseScreen.java
+++ PrgAndroidApps/LimeApp/app/src/main/java/com/example/limeapp/PriseScreen.java
@@ -3,10 +3,17 @@ package com.example.limeapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PriseScreen extends AppCompatActivity {
 
@@ -19,6 +26,175 @@ public class PriseScreen extends AppCompatActivity {
         ImageView profBut = findViewById(R.id.ProfButPrise);
         ImageView mainBut = findViewById(R.id.DocButPrise);
         ImageView PMSp1 = findViewById(R.id.pmsp1);
+        ImageView MainInfoBut1 = findViewById(R.id.imageView28);
+        ImageView MainInfoBut2 = findViewById(R.id.imageView23);
+        MainInfoBut2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(PriseScreen.this);
+                dialog.setContentView(R.layout.activity_main_info);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                ImageView closeButton = dialog.findViewById(R.id.imageView39);
+                dialog.show();
+
+                ImageView mapBut = dialog.findViewById(R.id.imageView43);
+                ImageView instBut = dialog.findViewById(R.id.imageView50);
+                FloatingActionButton floatingActionButton2 = dialog.findViewById(R.id.floatingActionButton2);
+                ImageView faceBut = dialog.findViewById(R.id.imageView51);
+                FloatingActionButton floatingActionButton = dialog.findViewById(R.id.floatingActionButton);
+
+
+                ImageView num1 = dialog.findViewById(R.id.imageView48);
+                ImageView num2 = dialog.findViewById(R.id.imageView49);
+                num2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String dial = "tel:" + "0953836588";
+                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                    }
+                });
+                num1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String dial = "tel:" + "0535556530";
+                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                    }
+                });
+                floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        floatingActionButton2.setVisibility(View.INVISIBLE);
+                        instBut.setVisibility(View.VISIBLE);
+                        faceBut.setVisibility(View.VISIBLE);
+                        Animation fadeInAnimation = AnimationUtils.loadAnimation(PriseScreen.this, android.R.anim.fade_in);
+                        Animation fadeOutAnimation1 = AnimationUtils.loadAnimation(PriseScreen.this, android.R.anim.slide_out_right);
+                        instBut.startAnimation(fadeInAnimation);
+                        instBut.startAnimation(fadeInAnimation);
+                        floatingActionButton2.startAnimation(fadeOutAnimation1);
+                    }
+                });
+
+                faceBut.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.facebook.com/profile.php?id=100090329526191"));
+                        startActivity(browserIntent);
+                    }
+                });
+                instBut.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://instagram.com/lime_myrhorod?igshid=NTc4MTIwNjQ2YQ=="));
+                        startActivity(browserIntent);
+                    }
+                });
+                floatingActionButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String dial = "tel:" + "0953836588";
+                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                    }
+                });
+                mapBut.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.app.goo.gl/WFztc1xyYPfoGPHr6"));
+                        startActivity(browserIntent);
+                    }
+                });
+
+
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        MainInfoBut1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(PriseScreen.this);
+                dialog.setContentView(R.layout.activity_main_info);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                ImageView closeButton = dialog.findViewById(R.id.imageView39);
+                dialog.show();
+
+                ImageView mapBut = dialog.findViewById(R.id.imageView43);
+                ImageView instBut = dialog.findViewById(R.id.imageView50);
+                ImageView faceBut = dialog.findViewById(R.id.imageView51);
+                FloatingActionButton floatingActionButton = dialog.findViewById(R.id.floatingActionButton);
+                FloatingActionButton floatingActionButton2 = dialog.findViewById(R.id.floatingActionButton2);
+                ImageView num1 = dialog.findViewById(R.id.imageView48);
+                ImageView num2 = dialog.findViewById(R.id.imageView49);
+                num2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String dial = "tel:" + "0953836588";
+                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                    }
+                });
+                num1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String dial = "tel:" + "0535556530";
+                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                    }
+                });
+                floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        floatingActionButton2.setVisibility(View.INVISIBLE);
+                        instBut.setVisibility(View.VISIBLE);
+                        faceBut.setVisibility(View.VISIBLE);
+                        Animation fadeInAnimation = AnimationUtils.loadAnimation(PriseScreen.this, android.R.anim.fade_in);
+                        Animation fadeOutAnimation1 = AnimationUtils.loadAnimation(PriseScreen.this, android.R.anim.slide_out_right);
+                        instBut.startAnimation(fadeInAnimation);
+                        instBut.startAnimation(fadeInAnimation);
+                        floatingActionButton2.startAnimation(fadeOutAnimation1);
+                    }
+                });
+
+                faceBut.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.facebook.com/profile.php?id=100090329526191"));
+                        startActivity(browserIntent);
+                    }
+                });
+                instBut.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://instagram.com/lime_myrhorod?igshid=NTc4MTIwNjQ2YQ=="));
+                        startActivity(browserIntent);
+                    }
+                });
+                floatingActionButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String dial = "tel:" + "0953836588";
+                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                    }
+                });
+                mapBut.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.app.goo.gl/WFztc1xyYPfoGPHr6"));
+                        startActivity(browserIntent);
+                    }
+                });
+
+
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
         profBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- PrgAndroidApps/LimeApp/app/src/main/java/com/example/limeapp/ProfileActivity.java
+++ PrgAndroidApps/LimeApp/app/src/main/java/com/example/limeapp/ProfileActivity.java
@@ -1,10 +1,14 @@
 package com.example.limeapp;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -17,6 +21,7 @@ import androidx.appcompat.app.AppCompatDelegate;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -65,6 +70,175 @@ public class ProfileActivity extends AppCompatActivity {
         ImageView SavedButton = findViewById(R.id.saved_button);
         circleImageView = findViewById(R.id.profile_image);
         ImageView addImage = findViewById(R.id.imageView34);
+        ImageView MainInfoBut1 = findViewById(R.id.imageView28);
+        ImageView MainInfoBut2 = findViewById(R.id.imageView23);
+        MainInfoBut2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(ProfileActivity.this);
+                dialog.setContentView(R.layout.activity_main_info);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                ImageView closeButton = dialog.findViewById(R.id.imageView39);
+                dialog.show();
+
+                ImageView mapBut = dialog.findViewById(R.id.imageView43);
+                ImageView instBut = dialog.findViewById(R.id.imageView50);
+                FloatingActionButton floatingActionButton2 = dialog.findViewById(R.id.floatingActionButton2);
+                ImageView faceBut = dialog.findViewById(R.id.imageView51);
+                FloatingActionButton floatingActionButton = dialog.findViewById(R.id.floatingActionButton);
+
+
+                ImageView num1 = dialog.findViewById(R.id.imageView48);
+                ImageView num2 = dialog.findViewById(R.id.imageView49);
+                num2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String dial = "tel:" + "0953836588";
+                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                    }
+                });
+                num1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String dial = "tel:" + "0535556530";
+                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                    }
+                });
+                floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        floatingActionButton2.setVisibility(View.INVISIBLE);
+                        instBut.setVisibility(View.VISIBLE);
+                        faceBut.setVisibility(View.VISIBLE);
+                        Animation fadeInAnimation = AnimationUtils.loadAnimation(ProfileActivity.this, android.R.anim.fade_in);
+                        Animation fadeOutAnimation1 = AnimationUtils.loadAnimation(ProfileActivity.this, android.R.anim.slide_out_right);
+                        instBut.startAnimation(fadeInAnimation);
+                        instBut.startAnimation(fadeInAnimation);
+                        floatingActionButton2.startAnimation(fadeOutAnimation1);
+                    }
+                });
+
+                faceBut.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.facebook.com/profile.php?id=100090329526191"));
+                        startActivity(browserIntent);
+                    }
+                });
+                instBut.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://instagram.com/lime_myrhorod?igshid=NTc4MTIwNjQ2YQ=="));
+                        startActivity(browserIntent);
+                    }
+                });
+                floatingActionButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String dial = "tel:" + "0953836588";
+                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                    }
+                });
+                mapBut.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.app.goo.gl/WFztc1xyYPfoGPHr6"));
+                        startActivity(browserIntent);
+                    }
+                });
+
+
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        MainInfoBut1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(ProfileActivity.this);
+                dialog.setContentView(R.layout.activity_main_info);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                ImageView closeButton = dialog.findViewById(R.id.imageView39);
+                dialog.show();
+
+                ImageView mapBut = dialog.findViewById(R.id.imageView43);
+                ImageView instBut = dialog.findViewById(R.id.imageView50);
+                ImageView faceBut = dialog.findViewById(R.id.imageView51);
+                FloatingActionButton floatingActionButton = dialog.findViewById(R.id.floatingActionButton);
+                FloatingActionButton floatingActionButton2 = dialog.findViewById(R.id.floatingActionButton2);
+                ImageView num1 = dialog.findViewById(R.id.imageView48);
+                ImageView num2 = dialog.findViewById(R.id.imageView49);
+                num2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String dial = "tel:" + "0953836588";
+                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                    }
+                });
+                num1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String dial = "tel:" + "0535556530";
+                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                    }
+                });
+                floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        floatingActionButton2.setVisibility(View.INVISIBLE);
+                        instBut.setVisibility(View.VISIBLE);
+                        faceBut.setVisibility(View.VISIBLE);
+                        Animation fadeInAnimation = AnimationUtils.loadAnimation(ProfileActivity.this, android.R.anim.fade_in);
+                        Animation fadeOutAnimation1 = AnimationUtils.loadAnimation(ProfileActivity.this, android.R.anim.slide_out_right);
+                        instBut.startAnimation(fadeInAnimation);
+                        instBut.startAnimation(fadeInAnimation);
+                        floatingActionButton2.startAnimation(fadeOutAnimation1);
+                    }
+                });
+
+                faceBut.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.facebook.com/profile.php?id=100090329526191"));
+                        startActivity(browserIntent);
+                    }
+                });
+                instBut.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://instagram.com/lime_myrhorod?igshid=NTc4MTIwNjQ2YQ=="));
+                        startActivity(browserIntent);
+                    }
+                });
+                floatingActionButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String dial = "tel:" + "0953836588";
+                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                    }
+                });
+                mapBut.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.app.goo.gl/WFztc1xyYPfoGPHr6"));
+                        startActivity(browserIntent);
+                    }
+                });
+
+
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
 
         storageProfileReference = FirebaseStorage.getInstance().getReference().child("Profile Pic");
 
